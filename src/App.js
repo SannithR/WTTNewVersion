175bb13fe31238d9e5d60fb3cc74a6ad8bbd8bda
Sannith <sannithpeddolla143@gmail.com>
2025-05-08 12:08:26 +0530

    instructions modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import './App.css';
 import { HomepageEnglish } from './HomepageEnglish/HomepageEnglish';
 import { LanguageProvider } from './context/LanguageContext';
-import { Draws } from './Draws/DrawsComponents/DrawsComponents';
-import { Results } from './Results/ResultsComponent/ResultsComponent';
-import { Schedule } from './Schedule/ScheduleComponent/ScheduleComponent';
-import { Players } from './Players/Players/Players';
-
-
-
-
 
 function App() {
   return (
